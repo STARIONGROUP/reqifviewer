85bfd146e6456e5877ac3472fcccdf500bb0162c
sam.gerene <s.gerene@rheagroup.com>
2021-11-12 18:11:02 +0100

    [Cleanup] project and add copyright headers

--- reqifviewer/Program.cs
+++ reqifviewer/Program.cs
@@ -25,23 +25,25 @@
 namespace reqifviewer
 {
     using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.Logging;
-    using System;
-    using System.Collections.Generic;
-    using System.Net.Http;
-    using System.Text;
     using System.Threading.Tasks;
 
+    /// <summary>
+    /// The purpose of the <see cref="Program"/> class is to provide the
+    /// main entry point of te application
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Main entry point of the application
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
+        /// <returns>
+        /// an awaitable <see cref="Task"/>
+        /// </returns>
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
         }
